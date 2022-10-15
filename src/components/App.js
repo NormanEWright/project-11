befecaccd1c8d005928536d6a2c5cf579e65ebc5
= <n.wright.online@gmail.com>
2022-10-15 16:31:24 -0500

    Modify api data, refactor components

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect, useCallback } from 'react';
 import '../blocks/App.css';
 import Header from './Header';
 import Main from './Main';
-import WeatherCard from './WeatherCard';
-import ItemCard from './ItemCard';
 import ModalWithForm from './ModalWithForm';
 import NewGarmentForm from './NewGarmentForm';
 import Footer from './Footer';
@@ -12,26 +10,30 @@ import ItemModal from './ItemModal';
 import WeatherApi, { getWeatherTemperature } from '../utils/WeatherApi';
 import { useGlobalKeydownListener } from '../hooks/useGlobalKeydownListener';
 import { useOutsideClickListener } from '../hooks/useOutsideClickListener';
+import { CurrentTemperatureUnitContext } from '../contexts/CurrentTemperatureUnitContext';
 
 const api = new WeatherApi();
 
 function App() {
-  const [temperature, setTemperature] = useState("");
-  const [weather, setWeather] = useState("");
-  const [location, setLocation] = useState("Kingston");
-  const [isDay, setIsDay] = useState(1);
+  const [weather, setWeather] = useState({});
   const [isNewGarmentModalOpen, setIsNewGarmentModalOpen] = useState(false);
   const [isPreviewModalOpen, setIsPreviewModalOpen] = useState(false);
   const [itemData, setItemData] = useState({link: '', title: '', descriptions: ''});
+  const [currentTemperatureUnit, setCurrentTemperatureUnit] = useState('F');
 
   useEffect(() => {
     api
       .getCurrentWeather("18.0179, -76.8099")
-      .then((currentWeather) => {
-        setWeather(currentWeather.current.condition.text);
-        setLocation(`${currentWeather.location.name}`);
-        setIsDay(currentWeather.current.is_day);
-        setTemperature(Math.round(currentWeather.current.temp_f));
+      .then((data) => {
+        const weatherData = {
+          temp_f: `${data.current.temp_f}°F`, 
+          temp_c: `${data.current.temp_c}°C`,
+          range: getWeatherTemperature(data.current.temp_f),
+          location: data.location.name,
+          is_day: data.current.is_day,
+          condition: data.current.condition.text,
+        }
+        setWeather(weatherData);
       })
       .catch((err) => {
         console.log(err);
@@ -55,52 +57,23 @@ function App() {
   useGlobalKeydownListener('Escape', closeModals);
   useOutsideClickListener('popup', closeModals);
 
+  const handleToggleSwitchChange = () => {
+    currentTemperatureUnit === 'F'
+      ? setCurrentTemperatureUnit('C')
+      : setCurrentTemperatureUnit('F');
+  };
+
   return (
     <div className='App'>
-      <Header
-        username="Norman Wright"
-        name="add"
-        currentDate={currentDate}
-        currentLocation={location}
-        openPopup={openNewGarmentModal}
-        onClose={closeModals}
-      />
-      <Main temp={temperature} >
-        <WeatherCard
-          id="weather"
-          key="weatherCard"
-          temp={temperature}
-          weather={weather}
-          isDay={isDay}
-        />
-          <ul className='card__list'>
-            {
-              defaultClothingItems.map(item => <ItemCard
-                key={item._id}
-                data={item}
-                openPopup={openPreviewModal}
-                currentWeather={getWeatherTemperature(temperature)}
-              />)
-            }
-          </ul>
-      </Main>
-      <ModalWithForm
-        title="New garment"
-        id="addGarment"
-        name="add"
-        buttonText="Add garment"
-        isOpen={isNewGarmentModalOpen}
-        onClose={closeModals}
-      >
-        <NewGarmentForm />
-      </ModalWithForm>
-      <ItemModal
-        name="item"
-        isOpen={isPreviewModalOpen}
-        onClose={closeModals}
-        data={itemData}
-      />
-      <Footer />
+      <CurrentTemperatureUnitContext.Provider value={{ currentTemperatureUnit, handleToggleSwitchChange }}>
+        <Header username="Norman Wright" name="add" currentDate={currentDate} currentLocation={weather.location} openPopup={openNewGarmentModal} onClose={closeModals} />
+        <Main weather={weather} clothingItems={defaultClothingItems} openPopup={openPreviewModal} />
+        <ModalWithForm title="New garment" id="addGarment" name="add" buttonText="Add garment" isOpen={isNewGarmentModalOpen} onClose={closeModals} >
+          <NewGarmentForm />
+        </ModalWithForm>
+        <ItemModal name="item" isOpen={isPreviewModalOpen} onClose={closeModals} data={itemData} />
+        <Footer />
+      </CurrentTemperatureUnitContext.Provider>
     </div>
   );
 }
