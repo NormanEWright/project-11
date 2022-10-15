05248122d5466d383927cbc5a9bfd5d965a5a3e2
= <n.wright.online@gmail.com>
2022-10-15 16:32:27 -0500

    Modify components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import "../blocks/Main.css";
 import "../blocks/card.css";
+import WeatherCard from './WeatherCard';
+import ItemCard from './ItemCard';
+import { CurrentTemperatureUnitContext } from '../contexts/CurrentTemperatureUnitContext';
 
-function Main(props) {
+function Main({ weather, clothingItems, openPopup }) {
+  const { currentTemperatureUnit } = React.useContext(CurrentTemperatureUnitContext);
+
   return (
     <main className="main">
-      {props.children[0]}
+      <WeatherCard weather={ weather } />
       <section className="card__section">
-        <h3 className="card__heading">Today is {props.temp} °F / You may want to wear:</h3>
-        {props.children[1]}
+        <h3 className="card__heading">Today is {currentTemperatureUnit === 'F' ? weather.temp_f : weather.temp_c} / You may want to wear:</h3>
+        <ul className='card__list'>
+          {
+            clothingItems.map(item => <ItemCard
+              key={item._id}
+              data={item}
+              openPopup={openPopup}
+              currentWeather={weather.range}
+            />)
+          }
+        </ul>
       </section>
     </main>
   )
