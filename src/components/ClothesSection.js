7b14a802e83b6c15679599921128da8ffb55dc48
= <n.wright.online@gmail.com>
2022-10-15 22:05:51 -0500

    Create ClothesSection component

---
+++ src/components/ClothesSection.js
@@ -0,0 +1,27 @@
+import React from "react";
+import '../blocks/Profile.css';
+import "../blocks/card.css";
+import ItemCard from "./ItemCard";
+
+function ClothesSection({ weather, clothingItems, openPopup }) {
+  return (
+    <div className="profile__clothes-section">
+      <div className="profile__main-header">
+        <p className="profile__clothes-section-title">Your items</p>
+        <button className="profile__add-button" onClick={openPopup}>+ Add new</button>
+      </div>
+      <ul className="profile__card-list">
+        {
+          clothingItems.map(item => <ItemCard
+            key={item._id}
+            data={item}
+            openPopup={openPopup}
+            currentWeather={weather.range}
+          />)
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default ClothesSection;
