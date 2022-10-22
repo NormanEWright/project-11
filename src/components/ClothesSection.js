7a11f0a3e1acd8868a15f558ea3e73ba80e3f65f
= <n.wright.online@gmail.com>
2022-10-22 15:05:43 -0500

    Modify onClick handler to add new items

--- src/components/ClothesSection.js
+++ src/components/ClothesSection.js
@@ -3,12 +3,12 @@ import '../blocks/Profile.css';
 import "../blocks/card.css";
 import ItemCard from "./ItemCard";
 
-function ClothesSection({ weather, clothingItems, openPopup }) {
+function ClothesSection({ weather, clothingItems, openPopup, openPopupWithForm }) {
   return (
     <div className="profile__clothes-section">
       <div className="profile__main-header">
         <p className="profile__clothes-section-title">Your items</p>
-        <button className="profile__add-button" onClick={openPopup}>+ Add new</button>
+        <button className="profile__add-button" onClick={openPopupWithForm}>+ Add new</button>
       </div>
       <ul className="profile__card-list">
         {
