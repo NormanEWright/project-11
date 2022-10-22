831b56e11e71efa9fa48ae1dc59092970d13f1e1
= <n.wright.online@gmail.com>
2022-10-22 17:25:02 -0500

    Create new module to get temperature range

---
+++ src/utils/getWeatherTemperature.js
@@ -0,0 +1,11 @@
+function getWeatherTemperature(temperature) {
+  if (temperature >= 86) {
+    return 'hot';
+  } else if (temperature >= 66 && temperature <= 85) {
+    return 'warm';
+  } else if (temperature <= 65) {
+    return 'cold';
+  }
+}
+
+export default getWeatherTemperature;
