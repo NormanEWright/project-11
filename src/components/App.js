ce7fb77d2214cbc3cf32a865e039d126145fc996
= <n.wright.online@gmail.com>
2022-10-22 17:34:51 -0500

    Refactor call to weather api, format components

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,9 @@ import NewGarmentForm from './NewGarmentForm';
 import AddItemModal from './AddItemModal';
 import DeleteConfirmationModal from './DeleteConfirmationModal';
 import Footer from './Footer';
-import { currentDate, isDayTime } from '../utils/constants';
+import { currentDate } from '../utils/constants';
 import ItemModal from './ItemModal';
-import WeatherApi, { getWeatherTemperature } from '../utils/WeatherApi';
+import WeatherApi from '../utils/WeatherApi';
 import { useGlobalKeydownListener } from '../hooks/useGlobalKeydownListener';
 import { useOutsideClickListener } from '../hooks/useOutsideClickListener';
 import { CurrentTemperatureUnitContext } from '../contexts/CurrentTemperatureUnitContext';
@@ -33,17 +33,7 @@ function App() {
     api
       .getCurrentWeather("18.0179, -76.8099")
       .then((data) => {
-        console.log(data);
-        const weatherData = {
-          temp_f: `${data.main.temp}°F`, 
-          temp_c: `${Math.round((data.main.temp - 32) * 5/9)}°C`,
-          range: getWeatherTemperature(data.main.temp),
-          location: data.name,
-          isDayTime: isDayTime,
-          condition: data.weather[0].description,
-        }
-        setWeather(weatherData);
-        console.log(weatherData);
+        setWeather(data);
       })
       .catch((err) => {
         console.log(err);
@@ -110,60 +100,21 @@ function App() {
     <div className='App'>
       <CurrentTemperatureUnitContext.Provider value={{ currentTemperatureUnit, handleToggleSwitchChange }}>
         <BrowserRouter>
-          <Header
-            username="Norman Wright"
-            name="add"
-            currentDate={currentDate}
-            currentLocation={weather.location}
-            openPopup={openNewGarmentModal}
-            onClose={closeModals}
-          />
+          <Header username="Norman Wright" name="add" currentDate={currentDate} currentLocation={weather.location} openPopup={openNewGarmentModal} onClose={closeModals} />
           <Switch>
             <Route exact path="/">
-              <Main
-                weather={weather}
-                clothingItems={clothingList}
-                openPopup={openPreviewModal}
-              />
+              <Main weather={weather} clothingItems={clothingList} openPopup={openPreviewModal} />
             </Route>
             <Route path="/profile">
-              <Profile
-                user="Norman Wright"
-                weather={weather}
-                clothingItems={clothingList}
-                openPopup={openPreviewModal}
-                openPopupWithForm={openNewGarmentModal}
-                addNewItem={handleAddNewItemClick}
-                isOpen={isAddItemModalOpen}
-              />
+              <Profile user="Norman Wright" weather={weather} clothingItems={clothingList} openPopup={openPreviewModal} openPopupWithForm={openNewGarmentModal} addNewItem={handleAddNewItemClick} isOpen={isAddItemModalOpen} />
             </Route>
           </Switch>
-          <ModalWithForm
-            title="New garment"
-            id="addGarment"
-            name="add"
-            buttonText="Add garment"
-            isOpen={isNewGarmentModalOpen}
-            onClose={closeModals}
-          >
+          <ModalWithForm title="New garment" id="addGarment" name="add" buttonText="Add garment" isOpen={isNewGarmentModalOpen} onClose={closeModals}>
             <NewGarmentForm />
           </ModalWithForm>
-          <ItemModal
-            name="item"
-            isOpen={isPreviewModalOpen}
-            onClose={closeModals}
-            data={itemData}
-            confirmDelete={openConfirmationModal}
-          />
-          <AddItemModal
-            onAddItem={handleAddItemSubmit}
-          />
-          <DeleteConfirmationModal
-            name="confirm"
-            isOpen={isConfirmModalOpen}
-            onClose={closeModals}
-            confirmDeletePopup={handleCardDelete}
-          />
+          <ItemModal name="item" isOpen={isPreviewModalOpen} onClose={closeModals} data={itemData} confirmDelete={openConfirmationModal} />
+          <AddItemModal onAddItem={handleAddItemSubmit} />
+          <DeleteConfirmationModal name="confirm" isOpen={isConfirmModalOpen} onClose={closeModals} confirmDeleteCard={handleCardDelete} />
           <Footer />
         </BrowserRouter>
       </CurrentTemperatureUnitContext.Provider>
