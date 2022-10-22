42d4b6614160487b0943355955d812b6110471b5
= <n.wright.online@gmail.com>
2022-10-22 17:24:18 -0500

    Extract and return weather data, remove function

--- src/utils/WeatherApi.js
+++ src/utils/WeatherApi.js
@@ -1,14 +1,7 @@
 import React from 'react';
+import { isDayTime } from '../utils/constants';
+import getWeatherTemperature from './getWeatherTemperature';
 
-export function getWeatherTemperature(temperature) {
-  if (temperature >= 86) {
-    return 'hot';
-  } else if (temperature >= 66 && temperature <= 85) {
-    return 'warm';
-  } else if (temperature <= 65) {
-    return 'cold';
-  }
-}
 
 class WeatherApi extends React.Component {
   _processResponse = (res) => {
@@ -20,7 +13,23 @@ class WeatherApi extends React.Component {
   
   async getCurrentWeather(parsedLocation) {
     const res = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=18.0179&lon=-76.8099&appid=cd2258b79f5b2bfac5d80485ea366632&units=imperial');
-    return this._processResponse(res);
+    return this._processResponse(res)
+    .then((res) => {
+      console.log(Math.round(res.main.temp));
+      const data = {
+        temp_f: `${Math.round(res.main.temp)}°F`,
+        temp_c: `${Math.round((res.main.temp - 32) * 5 / 9)}°C`,
+        range: getWeatherTemperature(Math.round(res.main.temp)),
+        location: res.name,
+        isDayTime: isDayTime,
+        condition: res.weather[0].description,
+      }
+      // console.log(data);
+      return data;
+    })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
