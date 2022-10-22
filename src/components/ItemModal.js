d153a701d0ac22989f6c3d2c4b3c5d7c87e0189f
= <n.wright.online@gmail.com>
2022-10-22 18:52:00 -0500

    Handle deleting an item

--- src/components/ItemModal.js
+++ src/components/ItemModal.js
@@ -3,15 +3,21 @@ import "../blocks/popup.css";
 import closeIcon from "../images/ItemModal/close-button.svg";
 import "../blocks/item-modal.css";
 
-function ItemModal({ name, isOpen, onClose, data, confirmDelete }) {
+function ItemModal({ name, isOpen, closePopup, data, openConfirmationModal, setDeleteItem }) {
+
+  const deleteItem = () => {
+    setDeleteItem(data.id);
+    openConfirmationModal();
+  }
+
   return(
     <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__item-wrapper">
         <img className="popup__item-image" src={data.link} alt={data.title} />
         <p className="popup__item-title" >{data.title}</p>
         <p className="popup__item-description" >Weather: {data.description}</p>
-        <button type="button" className="popup__delete-button" onClick={confirmDelete}>Delete</button>
-        <button type="button" className="popup__close-button" onClick={onClose}>
+        <button type="button" className="popup__delete-button" onClick={deleteItem}>Delete</button>
+        <button type="button" className="popup__close-button" onClick={closePopup}>
           <img className="popup__close-icon" src={closeIcon} alt="Close popup button" />
         </button>
       </div>
