42296f02d00fcf1072afecb19670cfb5a0b8f33b
= <n.wright.online@gmail.com>
2022-10-22 17:35:56 -0500

    Add onClick handler to confirm delete card

--- src/components/DeleteConfirmationModal.js
+++ src/components/DeleteConfirmationModal.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import closeIcon from "../images/ModalWithForm/close-button.svg";
 
-function DeleteConfirmationModal({ name, isOpen, onClose, confirmDeletePopup }) {
+function DeleteConfirmationModal({ name, isOpen, onClose, confirmDeleteCard }) {
   return (
     <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__delete-item-wrapper">
         <form className='popup__delete-item-form'>
           <p className="popup__confirm-text">Are you sure you want to delete this item?<br />This action is irreversible.</p>
-          <button type="button" className="popup__confirm-delete-button">Yes, delete item</button>
+          <button type="button" className="popup__confirm-delete-button" onClick={confirmDeleteCard}>Yes, delete item</button>
           <button type="button" className="popup__cancel-button" onClick={onClose}>Cancel</button>
           <button type="button" className="popup__close-button" onClick={onClose}>
             <img className="popup__close-icon" src={closeIcon} alt="Close popup button" />
