703e7558b9f9385a691b5e124b21bdd9e48cdb85
= <n.wright.online@gmail.com>
2022-10-23 19:22:10 -0500

    Add new method to handle requests

--- src/utils/WeatherApi.js
+++ src/utils/WeatherApi.js
@@ -3,7 +3,6 @@ import { isDayTime, key } from '../utils/constants';
 import getWeatherTemperature from './getWeatherTemperature';
 import { coords } from '../utils/constants';
 
-
 class WeatherApi extends React.Component {
   _processResponse = (res) => {
     if (res.ok) {
@@ -11,21 +10,24 @@ class WeatherApi extends React.Component {
     }
     return Promise.reject(`Error: ${res.status}`);
   }
-  
-  async getCurrentWeather(parsedLocation) {
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.long}&lon=${coords.lat}&appid=${key}&units=imperial`);
-    return this._processResponse(res)
-    .then((res) => {
-      const data = {
-        temp_f: `${Math.round(res.main.temp)}°F`,
-        temp_c: `${Math.round((res.main.temp - 32) * 5 / 9)}°C`,
-        range: getWeatherTemperature(Math.round(res.main.temp)),
-        location: res.name,
-        isDayTime: isDayTime,
-        condition: res.weather[0].description,
-      }
-      return data;
-    })
+
+  _request(url) {
+    return fetch(url).then(this._processResponse)
+  }
+
+  getCurrentWeather(parsedLocation) {
+    return this._request(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.long}&lon=${coords.lat}&appid=${key}&units=imperial`)
+      .then((res) => {
+        const data = {
+          temp_f: `${Math.round(res.main.temp)}°F`,
+          temp_c: `${Math.round((res.main.temp - 32) * 5 / 9)}°C`,
+          range: getWeatherTemperature(Math.round(res.main.temp)),
+          location: res.name,
+          isDayTime: isDayTime,
+          condition: res.weather[0].description,
+        }
+        return data;
+      })
   }
 }
 
