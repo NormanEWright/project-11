93276456502b3fee5e08ca7fe995bd6e343fef53
= <n.wright.online@gmail.com>
2022-10-25 18:44:01 -0500

    Remove comments

--- src/components/ModalWithForm.js
+++ src/components/ModalWithForm.js
@@ -4,17 +4,12 @@ import closeIcon from "../images/ModalWithForm/close-button.svg";
 
 function ModalWithForm({ name, title, children, buttonText, isOpen, closePopup, handleSubmit }) {
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   closePopup();
-  // }
-
   return(
     <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>
-      <form action="submit" className="popup__form" id="add-garment-form" noValidate>
+      <form action="submit" className="popup__form" id="add-garment-form" onSubmit={handleSubmit} >
         <h3 className="popup__form-heading">{title}</h3>
         {children}
-        <button type="submit" className="popup__form-add-button" onClick={handleSubmit} >{buttonText}</button>
+        <button type="submit" className="popup__form-add-button" >{buttonText}</button>
         <button type="button" className="popup__close-button" onClick={closePopup}>
           <img className="popup__close-icon" src={closeIcon} alt="Close popup button" />
         </button>
